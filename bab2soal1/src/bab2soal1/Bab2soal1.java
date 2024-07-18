3afa2dedd35eabf4771e85e09a7a07a9b45e4ef1
Alrifbar <144987510+Alrifbar@users.noreply.github.com>
2024-07-18 14:00:33 +0700

    Add files via upload

---
+++ bab2soal1/src/bab2soal1/Bab2soal1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab2soal1;
+
+/**
+ *
+ * @author supri
+ */
+public class Bab2soal1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){   
+int a=50;   
+float b=a;   
+System.out.println(a);   
+System.out.println(b);   
+}
+    
+}
---
+++ bab2soal2/src/bab2soal2/Bab2soal2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab2soal2;
+
+/**
+ *
+ * @author supri
+ */
+public class Bab2soal2 {
+
+       public static String Hobi; 
+//Membuat variable tanpa Value/Nilai pada class  staticvariable 
+   public static final String Hewan = "jus jambu";
+
+   public static void main(String[] args) { 
+// TODO code application logic here 
+Hobi = "Ayam Geprek ";//Mengisi Value pada variable Hobi 
+System.out.println("Makanan favorit Saya " + Hobi);//Memanggil Variable   Hobi 
+System.out.println("Minuman favorit Saya "+Hewan);//Memanggil Variable Hewan
+        }
+}
